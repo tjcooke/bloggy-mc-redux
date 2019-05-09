93e67c14a34d7e067aea592b81e48331c90742c0
Tyler Cooke <tylerstouchpad@gmail.com>
2019-05-09 09:59:27 -0400

    adds store that uses posts reducer

---
+++ src/reducers/posts.js
@@ -0,0 +1,13 @@
+import {
+    ACTION_CREATE_POST
+} from '../actions'
+
+
+export function posts(state={}, action={type: ''}) {
+
+    switch(action.type) {
+        case ACTION_CREATE_POST:
+        // create a post! 
+        break;
+    }
+}
